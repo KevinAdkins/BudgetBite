8b0a3d7362e0dad5f10623cbb3d33eb6119f796e
dabun01 <dabun01@jaguar.tamu.edu>
2026-02-21 16:03:25 -0600

    Created frontend files and added pantry tab to the app.

---
+++ frontend/app/(tabs)/_layout.tsx
@@ -0,0 +1,29 @@
+import { Tabs } from 'expo-router';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+
+export default function TabLayout() {
+  return (
+    <Tabs
+        screenOptions={{
+            tabBarActiveTintColor: '#ffd33d',
+            headerStyle: { backgroundColor: '#25292e' },
+            headerShadowVisible: false,
+            headerTitleStyle: { color: '#fff' },
+            tabBarStyle: { backgroundColor: '#25292e' },
+        }}
+    >
+      <Tabs.Screen 
+        name="index" 
+        options={{ 
+            title: 'Home', 
+            tabBarIcon: ({ color }) => 
+            <Ionicons name="home-outline" size={24} color={color} /> }} 
+       />
+      <Tabs.Screen 
+        name="pantry" 
+        options={{ title: 'Pantry', tabBarIcon: ({ color }) => 
+        <MaterialCommunityIcons name="fridge" size={24} color={color} /> }} />
+    </Tabs>
+  );
+}
