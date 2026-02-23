5c906246d5764681e79326b28130bb967ecdac83
KevinAdkins <kevin.adkins1903@gmail.com>
2026-02-23 16:48:24 -0600

    Refactor Index component to remove image picker
    Removed image picker functionality and replaced it with a text and link.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,48 +1,13 @@
-import * as ImagePicker from "expo-image-picker";
-import { StyleSheet, View } from "react-native";
-
-import { useState } from "react";
-
-import Button from "@/components/Button";
-import ImageViewer from "@/components/ImageViewer";
-
-const PlaceholderImage = require("@/assets/images/IMG_8688.png");
+import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
-  const [selectedImage, setSelectedImage] = useState<string | undefined>(
-    undefined,
-  );
-
-  const pickImageAsync = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ["images"],
-      allowsEditing: true,
-      quality: 1,
-    });
-
-    if (!result.canceled) {
-      setSelectedImage(result.assets[0].uri);
-    } else {
-      alert("You did not select any image.");
-    }
-  };
-
   return (
     <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <ImageViewer
-          imgSource={PlaceholderImage}
-          selectedImage={selectedImage}
-        />
-      </View>
-      <View style={styles.footerContainer}>
-        <Button
-          theme="primary"
-          label="Choose a photo"
-          onPress={pickImageAsync}
-        />
-        <Button label="Use this photo" />
-      </View>
+      <Text style={styles.text}>Hello World</Text>
+      <Link href="/pantry" style={styles.button}>
+        Go to Pantry Screen
+      </Link>
     </View>
   );
 }
@@ -52,12 +17,17 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#25292e",
     alignItems: "center",
+    justifyContent: "center",
   },
-  imageContainer: {
-    flex: 1,
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#fff",
   },
-  footerContainer: {
-    flex: 1 / 3,
-    alignItems: "center",
+  button: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: "#61dafb",
+    borderRadius: 5,
   },
 });
